5094070a77b884964b1695ec0eb475cf8bb376f8
n2go <n2go@logeecom.com>
2016-12-09 10:24:10 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Vagrant Box
+
+A super-simple Vagrantfile / bootstrap.sh to setup a LAMP stack inside Vagrant 100% automatically.
+
+### Whaaaaat ?
+
+This is a reduced-to-the-max Vagrant setup file for a quick development stack. It will:
+
+* setup a Ubuntu 14.04 LTS "Trustry Thar" 64bit box
+
+* make the box accessable by the host at IP `192.168.33.22` or localhost:8080
+
+* sync the current folder with `/vagrant` inside the box
+
+* automatically perform all the commands in bootstrap.sh directly after setting up the box for the first time
+
+The bootstrap.sh will:
+
+* update, upgrade
+
+* create a folder inside /vagrant
+
+* install apache 2.4, php 5.6, MySQL,Ioncube , git and Composer
+
+* also setting a pre-chosen password for MySQL
+
+* activate mod_rewrite and add *AllowOverride All* to the vhost settings
+
+You can folder and password inside the bootstrap.sh for sure.
+
+### How to use ?
+
+Put `Vagrantfile` and `bootstrap.sh` inside a folder and do a `vagrant up` on the command line.
+This box uses Ubuntu 14.04 LTS "Trustry Thar" 64bit, so if you don't have the basic box already, do a 
+`vagrant box add ubuntu/trusty64` before.
+
+### Why ?
+
+This is just my personal time-saving bootstrap for Vagrant, it might be useful for you too.
---
+++ bootstrap.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='root'
+PROJECTFOLDER='My Projects'
+
+# create project folder
+#sudo mkdir "/vagrant/${PROJECTFOLDER}"
+
+# update / upgrade
+# update / upgrade
+sudo apt-get install -y python-software-properties software-properties-common
+sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php 
+sudo apt-get purge -y php5-common
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5.6 php5.6-curl php5.6-soap
+sudo apt-get --purge -y autoremove 
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5.6-mysql
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+	PHPINIDir /vagrant
+    DocumentRoot "/vagrant/${PROJECTFOLDER}"
+    <Directory "/vagrant/${PROJECTFOLDER}">
+		Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-enabled/000-default.conf
+# php extensions
+sudo apt-get install -y php5.6-mbstring
+sudo apt-get install -y php5.6-gd
+sudo apt-get install -y php5.6-xml
+
+#ioncube installation
+sudo wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+sudo tar -zxvf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local/src/
+sudo mkdir /usr/local/ioncube
+cp /usr/local/src/ioncube/ioncube_loader_lin_5.6.so /usr/local/ioncube/
+cp /usr/local/src/ioncube/ioncube_loader_lin_5.6_ts.so  /usr/local/ioncube/
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+ sudo apt-get -y install git
+
+# install Composer
+ curl -s https://getcomposer.org/installer | php
+ mv composer.phar /usr/local/bin/composer
+
+#database import
+#sudo  mysql -u root -proot -e "create database xtc";
+#sudo mysql -u root -proot xtc < /vagrant/xtc.sql
+
+
+
